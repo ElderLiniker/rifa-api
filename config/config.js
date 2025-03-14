da953477887b274423994ded58a639448334d23f
ElderLiniker <eldervhs2013@hotmail.com>
2025-03-14 19:06:10 -0300

    eee

---
+++ config/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  development: {
+    username: "use",
+    password: "bdQfD78JhPDcSjkB2tALguEi9Ko7WqD2",
+    database: "rifa_database",
+    host: "dpg-cv9n55bqf0us73c9phv0-a.oregon-postgres.render.com",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
+};
